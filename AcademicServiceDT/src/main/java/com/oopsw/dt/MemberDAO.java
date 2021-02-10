e70ff999864ee66ff24be26513d0ec1cfb989b8b
NyeongB <kdtoaj14@ajou.ac.kr>
2021-02-10 14:41:53 +0900

    first

---
+++ AcademicServiceDT/src/main/java/com/oopsw/dt/MemberDAO.java
@@ -0,0 +1,16 @@
+package com.oopsw.dt;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberDAO {
+	public String selectDate();// throws Exception;	
+	public String login(@Param("memberId")String id, @Param("pw")String pw);
+//	public String login(String id, String pw) {
+//		if(id.equals("admin") && pw.equals("123"))
+//			return "°ü¸®ÀÚ";
+//		return null;
+//	}
+
+}
---
+++ AcademicServiceDT/src/main/java/com/oopsw/dt/MemberService.java
@@ -0,0 +1,21 @@
+package com.oopsw.dt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service	
+public class MemberService {
+
+	
+	@Autowired
+	private MemberDAO memberDAO;
+
+	public String selectDate(){
+		return memberDAO.selectDate();
+	}
+	public String login(String id, String pw) {
+		return memberDAO.login(id, pw);
+	}
+	
+}
