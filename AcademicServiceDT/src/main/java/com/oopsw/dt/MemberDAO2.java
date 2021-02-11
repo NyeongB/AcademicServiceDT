f3f3660266a77ab7a15bf59e19f4dcadd4bd6641
EastSun0106 <ehddkf0106@naver.com>
2021-02-11 15:50:30 +0900

    Controller, DAT, service2, 로그인

---
+++ AcademicServiceDT/src/main/java/com/oopsw/dt/MemberDAO2.java
@@ -0,0 +1,20 @@
+package com.oopsw.dt;
+
+import java.util.Collection;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberDAO2 {
+	public String login(@Param("studentId")String id, @Param("password")String pw);
+//	public String login(String id, String pw) {
+//		if(id.equals("admin") && pw.equals("123"))
+//			return "������";
+//		return null;
+//	}
+	public Collection<SubjectDTO> getAllSchedule();
+	
+	public Collection<SubjectDTO> getCheckApplysubject();
+
+}
---
+++ AcademicServiceDT/src/main/java/com/oopsw/dt/MemberService2.java
@@ -0,0 +1,28 @@
+package com.oopsw.dt;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service	
+public class MemberService2 {
+
+	
+	@Autowired
+	private MemberDAO2 memberDAO;
+
+	public String login(String id, String pw) {
+		return memberDAO.login(id, pw);
+	}
+	
+	public Collection<SubjectDTO> subjectList() {
+		return memberDAO.getAllSchedule();
+	}
+	
+	public Collection<SubjectDTO> checkApplySubject() {
+		return memberDAO.getCheckApplysubject();
+	}
+	
+}
