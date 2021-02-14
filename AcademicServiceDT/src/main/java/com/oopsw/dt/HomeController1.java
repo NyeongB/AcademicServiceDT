6b6764743c22c8bf0aebd64e09f9505aa22c30cb
KIMBYEONGHUI <byeonghui6253@gmail.com>
2021-02-14 14:37:29 +0900

    평점 계산

--- AcademicServiceDT/src/main/java/com/oopsw/dt/HomeController1.java
+++ AcademicServiceDT/src/main/java/com/oopsw/dt/HomeController1.java
@@ -17,111 +17,150 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.support.SessionStatus;
 
-
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController1 {
-	
+
 	@Autowired
 	private MemberService memberService;
-	
+
 	@RequestMapping(value = "/mySubjects", method = RequestMethod.GET) // 신청과목조회
 	public String checkApplySubject(HttpServletRequest request, Model model) {
-		
+
 		HttpSession session = request.getSession(true);
 		String id = (String) session.getAttribute("studentId");
-		
+
 		Collection<SubjectDTO> list = memberService.check(id);
-		request.setAttribute("list", list);
-		
-		return "04_my_subjects";
-	}
-	
-	@RequestMapping(value = "/delete", method = RequestMethod.GET) // 신청과목삭제
-	public String delete(HttpServletRequest request, Model model) {
-		
-		Integer num = memberService.delete(request.getParameter("registerCode"));
-		
-		HttpSession session = request.getSession(true);
-		String id = (String) session.getAttribute("studentId");
-		
-		Collection<SubjectDTO> list = memberService.check(id);
-		request.setAttribute("list", list);
-		
-		return "04_my_subjects";
-	}
-	
-	@RequestMapping(value = "/myScore", method = RequestMethod.GET) // 한 학기성적
-	public String scoreInfo(HttpServletRequest request, Model model) {
-		
-		HttpSession session = request.getSession(true);
-		String id = (String) session.getAttribute("studentId");
-		
-		Collection<ScoreDTO> list = memberService.score(id, request.getParameter("y"), request.getParameter("s"));
-		
-		//성적 계산
-		for(ScoreDTO dto : list){
-			int score = Integer.parseInt(dto.getScore());
-			dto.setScoreChange(calScore(score));
-			}
-		
 		request.setAttribute("list", list);
 
+		return "04_my_subjects";
+	}
+
+	@RequestMapping(value = "/delete", method = RequestMethod.GET) // 신청과목삭제
+	public String delete(HttpServletRequest request, Model model) {
+
+		Integer num = memberService.delete(request.getParameter("registerCode"));
+
+		HttpSession session = request.getSession(true);
+		String id = (String) session.getAttribute("studentId");
+
+		Collection<SubjectDTO> list = memberService.check(id);
+		request.setAttribute("list", list);
+
+		return "04_my_subjects";
+	}
+
+	@RequestMapping(value = "/myScore", method = RequestMethod.GET) // 한 학기성적
+	public String scoreInfo(HttpServletRequest request, Model model) {
+
+		HttpSession session = request.getSession(true);
+		String id = (String) session.getAttribute("studentId");
+
+		Collection<ScoreDTO> list = memberService.score(id, request.getParameter("y"), request.getParameter("s"));
+		
+		double total = 0.0;
+		int totalNum = 0;
+		// 성적 계산
+		for (ScoreDTO dto : list) {
+			totalNum += Integer.parseInt(dto.getCredit());
+			int score = Integer.parseInt(dto.getScore());
+			String score2 = calScore(score);
+			total += calAverage(score2) * Integer.parseInt(dto.getCredit());
+			dto.setScoreChange(score2);
+		}
+
+		request.setAttribute("list", list);
+		System.out.println(total / totalNum);
+		request.setAttribute("total", String.format("%.2f", total / totalNum) );
 		return "05_myScore";
 	}
+
 	@RequestMapping(value = "/myScores", method = RequestMethod.GET) // 전체성적
 	public String scoreInfo2(HttpServletRequest request, Model model) {
-		
+
 		HttpSession session = request.getSession(true);
 		String id = (String) session.getAttribute("studentId");
 
 		Collection<ScoreDTO> list = memberService.scoreAll(id);
 		
-		//성적 계산
-		for(ScoreDTO dto : list){
-			int score = Integer.parseInt(dto.getScore());	
-			dto.setScoreChange(calScore(score));
-			}
+		double total = 0.0;
+		int totalNum = 0;
+		// 성적 계산
+		for (ScoreDTO dto : list) {
+			totalNum += Integer.parseInt(dto.getCredit());
+			int score = Integer.parseInt(dto.getScore());
+			String score2 = calScore(score);
+			total += calAverage(score2) * Integer.parseInt(dto.getCredit());
+			dto.setScoreChange(score2);
+		}
 
 		request.setAttribute("list", list);
-		
+		System.out.println(total / totalNum);
+		request.setAttribute("total", String.format("%.2f", total / totalNum) );
 		return "05_myScore";
 	}
-	
+
 	@RequestMapping(value = "/start", method = RequestMethod.GET) // 첫화면
 	public String scoreInfo3(HttpServletRequest request, Model model) {
 
 		return "05_myScore";
 	}
-	
-	public String calScore(int score){ // 학점변환
-		
+
+	public String calScore(int score) { // 학점변환
+
 		String result = "";
-		
-		if(score > 95 && score <= 100){
+
+		if (score > 95 && score <= 100) {
 			result = "A+";
-		}
-		else if(score > 90 && score <= 95){
+		} else if (score > 90 && score <= 95) {
 			result = "A0";
-		}
-		else if(score > 85 && score <= 90){
+		} else if (score > 85 && score <= 90) {
 			result = "B+";
-		}
-		else if(score > 80 && score <= 85){
+		} else if (score > 80 && score <= 85) {
 			result = "B0";
-		}
-		else if(score > 75 && score <= 80){
+		} else if (score > 75 && score <= 80) {
 			result = "C+";
-		}
-		else if(score > 70 && score <= 75){
+		} else if (score > 70 && score <= 75) {
 			result = "C0";
-		}
-		else {
+		} else {
 			result = "F";
 		}
 		return result;
 	}
-	
+
+	public double calAverage(String score) { // 평점계산
+
+		double result = 0.0;
+
+		switch (score) {
+		case "A+":
+			result = 4.5;
+			break;
+		case "A0":
+			result = 4.0;
+			break;
+		case "B+":
+			result = 3.5;
+			break;
+		case "B0":
+			result = 3.0;
+			break;
+		case "C+":
+			result = 2.5;
+			break;
+		case "C0":
+			result = 2.0;
+			break;
+		case "F":
+			result = 0.0;
+			break;
+
+		}
+
+		return result;
+
+	}
+
 }
